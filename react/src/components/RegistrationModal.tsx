17879d929c0f80e2dc1255ff4b0f2ba633720d96
Teddy Juhlin-Henricson <teddy.juhlinhenricson@daresay.co>
2024-02-06 12:08:02 +0100

    Add blur behind the registration modal

--- react/src/components/RegistrationModal.tsx
+++ react/src/components/RegistrationModal.tsx
@@ -9,33 +9,42 @@ const RegistrationModal = ({ show, onClose }: RegistrationModalProps) => {
   if (!show) return null;
 
   return (
-    <div className="modal show" style={{ display: "block" }}>
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title">Register</h5>
-            <button
-              type="button"
-              className="btn-close"
-              onClick={onClose}
-            ></button>
-          </div>
-          <div className="modal-body">
-            {/* Registration form or content goes here */}
-            <p>Registration form</p>
-          </div>
-          <div className="modal-footer">
-            <button
-              type="button"
-              className="btn btn-secondary"
-              onClick={onClose}
-            >
-              Close
-            </button>
+    <>
+      <div
+        className={`custom-overlay ${show ? "d-block" : "d-none"}`}
+        style={{ display: "block" }}
+      ></div>
+      <div
+        className={`modal show ${show ? "d-block" : "d-none"}`}
+        style={{ display: "block" }}
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Register</h5>
+              <button
+                type="button"
+                className="btn-close"
+                onClick={onClose}
+              ></button>
+            </div>
+            <div className="modal-body">
+              {/* Registration form or content goes here */}
+              <p>Registration form</p>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={onClose}
+              >
+                Close
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 export default RegistrationModal;
