949c908d97bf3a550f630beb7295745a1424f826
Teddy Juhlin-Henricson <teddy.juhlinhenricson@daresay.co>
2024-02-06 09:40:23 +0100

    Initial commit and add React project with Vite

---
+++ react/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>SunkemonGo</h1>
+        <p>Login</p>
+        <p>Sign up</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
