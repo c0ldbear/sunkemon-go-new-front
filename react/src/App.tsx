8f3f144855851182b80b799c65b636157a6fd317
Teddy Juhlin-Henricson <teddy.juhlinhenricson@daresay.co>
2024-02-06 10:23:20 +0100

    Add input fields for email and password for login

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -5,7 +5,27 @@ function App() {
     <>
       <div>
         <h1>SunkemonGo</h1>
+
         <p>Login</p>
+        <div className="login-form-group">
+          <form className="login-form">
+            <input
+              className="input-container"
+              type="text"
+              id="email"
+              placeholder="Email"
+              name="email"
+            />
+
+            <input
+              className="input-container"
+              type="text"
+              id="password"
+              placeholder="Password"
+              name="password"
+            />
+          </form>
+        </div>
         <p>Sign up</p>
       </div>
     </>
