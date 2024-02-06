58d801ee29af7ca8bf695f67dc0497b6a3fb99cf
Teddy Juhlin-Henricson <teddy.juhlinhenricson@daresay.co>
2024-02-06 11:47:25 +0100

    Add chatgpt code

--- react/src/components/RegistrationModal.tsx
+++ react/src/components/RegistrationModal.tsx
@@ -1,7 +1,44 @@
 import React from "react";
 
-function RegistrationModal() {
-  return <div>RegistrationModal</div>;
-}
+type RegistrationModalProps = {
+  show: boolean;
+  onClose: () => void;
+};
 
+const RegistrationModal: React.FC<RegistrationModalProps> = ({
+  show,
+  onClose,
+}) => {
+  if (!show) return null;
+
+  return (
+    <div className="modal show" style={{ display: "block" }}>
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Register</h5>
+            <button
+              type="button"
+              className="btn-close"
+              onClick={onClose}
+            ></button>
+          </div>
+          <div className="modal-body">
+            {/* Registration form or content goes here */}
+            <p>Registration form</p>
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={onClose}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default RegistrationModal;
