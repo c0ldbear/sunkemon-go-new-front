a510e345631b7e4fa99e1b913769fc8f1d4ce79f
Teddy Juhlin-Henricson <teddy.juhlinhenricson@daresay.co>
2024-02-07 09:15:28 +0100

    Add Karl's help and fix some styling for log in card

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -11,7 +11,13 @@ function App() {
         className="d-flex justify-content-center align-items-center"
         style={{ minHeight: "80vh" }}
       >
-        <div className="container debug">
+        <div
+          className="card"
+          style={{
+            width: "500px",
+            boxShadow: "inset 0px 0px 10px rgba(0, 0, 0, 0.1)",
+          }}
+        >
           <h1>SunkemonGo</h1>
 
           <div className="mb-1 mt-3">
